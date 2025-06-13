42261d71f93b877ac4957d0865ec061f394e41c2
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2025-06-13 14:17:33 +0530

    adding go chi library

---
+++ social/cmd/api/api.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	log.Println("Starting server on", app.config.addr)
+	return srv.ListenAndServe()
+}
---
+++ social/go.mod
@@ -0,0 +1,5 @@
+module github.com/sinhadeepak1115/gophersbook/social
+
+go 1.23.5
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
